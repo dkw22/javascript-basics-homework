34d8093ec9bab69817cd1fcdec4c76a2a4196212
dkw22 <64745181+dkw22@users.noreply.github.com>
2021-06-28 15:24:00 -0400

    second commit

--- JavaScript-Homework.js
+++ JavaScript-Homework.js
@@ -27,16 +27,16 @@ console.log(findWords(dog_string,dog_names))
 /*Write a fucntion that takes in an array and removes every even index with a splice,
 and replaces it with the string "even index" */
 
-// let arr = ["Max","Baseball","Reboot","Goku","Trucks","Rodger"]
+let arr = ["Max","Baseball","Reboot","Goku","Trucks","Rodger"]
 
-// function replaceEvens(){
-//     for (let i = 0; i < arr.length; i+=2) {
-//         arr.splice(i, 1, 'even index')
-//     }
-//     return arr
-// }
+function replaceEvens(){
+    for (let i = 0; i < arr.length; i+=2) {
+        arr.splice(i, 1, 'even index')
+    }
+    return arr
+}
 
-// console.log(replaceEvens(arr))
+console.log(replaceEvens(arr))
 
 //Expected output
 //Given arr == ["Max","Baseball","Reboot","Goku","Trucks","Rodger"]
